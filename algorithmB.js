4c08b6adddd277858bf59f50ede65e905ca20570
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-03-12 20:01:53 +0100

    algoB finished and tests passing

--- algorithmB.js
+++ algorithmB.js
@@ -1,21 +1,19 @@
-//ordetIdinLista.filter((char) => char === Alfabetet[i]).length > 1
+/* 
+  Algorithm B
+  - The function takes in 3 parameters: a string of words, a number, and a boolean
+  - The function returns an array of words that are the same length as the number parameter
+  - If the boolean parameter is true, the function returns an array of words that are the same length as the number parameter
+  - If the boolean parameter is false, the function returns an array of words that are the same length as the number parameter and have no duplicate letters
+*/
 
 function algorithmB(words, length, hasDuplicates) {
-  words = words.split(' ')
-
+  //split the words into an array
+  words = words.split(' ');
+  //create an empty array called results which will be used to store the filtered words
   let results = [];
-  let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let characters = alphabet.split();
+  //create a variable called duplicates and set it to the value of the hasDuplicates parameter
   let duplicates = hasDuplicates;
-
-  function filterResults(currentValue, index) {
-    for (let j = 0; j < characters.length; j++) {
-      if (currentValue.split().filter((char) => char === characters[j]).length > 1) {
-        results.splice(index, 1);
-      }
-    }
-  }
-
+  // if the length parameter is set to a number, filter out words that are not the same length as the number parameter
   if (length) {
     for (let i = 0; i < words.length; i++) {
       if (words[i].length === length) {
@@ -23,14 +21,24 @@ function algorithmB(words, length, hasDuplicates) {
       }
     }
   }
-
+  // if the duplicates parameter is set to false, filter out words that have duplicate letters
   if (duplicates === false) {
-    results.forEach(filterResults);
+    results = results.filter((currentValue) => {
+      return currentValue.split('').every((char, index, self) => {
+        return self.indexOf(char) === index;
+      });
+    });
   }
-  return results;
+  // if the results array is empty, return an error message
+  if (results.length === 0) {
+    return 'No words were found with the given parameters. Please change the parameters and try again.';
+  }
+  // if the results array is not empty, return a random word from the results array
+  let randomizer = Math.floor(Math.random() * results.length);
+  return results[randomizer];
 }
 
-//let testrunB = algorithmB(7, false);
-//console.log(testrunB);
+let testrunB = algorithmB('passive monarch', 7, true);
+console.log(testrunB);
 
 export default algorithmB;
--- algorithmB.spec.js
+++ algorithmB.spec.js
@@ -1,22 +1,42 @@
-import algorithmB from "./algorithmB";
+import algorithmB from './algorithmB';
+
+/* Just as with my testing for algorithmA, I trued to work by writing my tests first. The order of my tests below is
+the same as the order i have broken down the algorithm into smaller pieces.
 
 /* making sure that the function is imported and working properly */
 test('algorithmB function is defined and is a function', () => {
-    expect(typeof algorithmB).toEqual('function');
-  });
+  expect(typeof algorithmB).toEqual('function');
+});
 
-  // testing to see if the the length parameter is working properly
-  test('making sure the length is correct', () => {
-    const result = algorithmB('prize possesion', 5, true);
-    expect(result).toEqual(['prize']);
-  });
+// testing to see if the the length parameter is working properly
+test('making sure the length is correct', () => {
+  const result = algorithmB('prize possesion', 5, true);
+  expect(result).toEqual('prize');
+});
 // testing to see if the duplicate parameter is working properly when set to its default value 'true'
-  test('test duplicate=true', () => {
-    const result = algorithmB('passive monarch', 7, true);
-    expect(result).toContain('passive');
-  });
-
-  test('test duplicate=false', () => {
-    const result = algorithmB('passive monarch', 7, false);
-    expect(result).not.toContain('passive');
-  });
+test('test duplicate=true', () => {
+  const result = algorithmB('passive monarch', 7, true);
+  expect(['passive', 'monarch'].some((el) => result.includes(el))).toBeTruthy();
+});
+// testing to see if the duplicate parameter is working properly when set to 'false'
+test('test duplicate=false', () => {
+  const result = algorithmB('passive monarch', 7, false);
+  expect(result).not.toContain('passive');
+});
+// testing to see if the randomizer is working properly
+test('test randomizer', () => {
+  const result = algorithmB('passive monarch cenarus randoms', 7, true);
+  //this line of code checks if the result contains any of the words in the array, admittedly this is probably not the best way to test this but it works for now.
+  expect(
+    ['passive', 'monarch', 'cenarus', 'randoms'].some((el) =>
+      result.includes(el)
+    )
+  ).toBeTruthy();
+});
+// testing to see if the function returns an error message if the result is empty
+test('test handling if result is empty', () => {
+  const result = algorithmB('passive monarch cenarus randoms', 3, true);
+  expect(
+    'No words were found with the given parameters. Please change the parameters and try again.'
+  );
+});
